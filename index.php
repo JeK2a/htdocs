7600bb94fe1816907b1cf8e1895a5166a80dd703
JeK-2a <vipjonpc@mail.ru>
2018-08-17 17:01:16 +0300

    Доработано

--- index.php
+++ index.php
@@ -8,18 +8,37 @@
 
 
 <div>
-    <form class="files_form">
-        <input type="file" multiple="multiple" name="files[]" accept="image" />
+    <form class="file_form" id="file_form" enctype="multipart/form-data" action="https://my.tdfort.ru/index.php?page=requests_edit_b&type=addFiles" method="POST">
+        <input type="submit" value="Отправить файлы" /><br>
+        <input type="file" multiple="multiple" name="0" /><br>
     </form>
 </div>
 
+<script>
 
+    let $file_load = function () {
 
+        $(document).on('change', 'input[type=file]', function() {
 
+            console.log(this.value);
+
+            let input = document.createElement('input');
+            input.type = 'file';
+            input.name = $('#file_form').find('input[type=file]').length;
+            let br = document.createElement('br')
+            document.getElementById('file_form').appendChild(input);
+            document.getElementById('file_form').appendChild(br);
+        });
+
+    };
+
+    $(document).ready(function () {
+        $file_load();
+    });
+
+</script>
 
 </body>
 </html>
-
-
 
 <?php
